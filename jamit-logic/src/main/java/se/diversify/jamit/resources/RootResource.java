38990288890823cfe22fe1b330159c8c2f22e530
Uzi Landsmann <uzi.landsmann@gmail.com>
2012-06-10 20:13:59 +0200

    prepared for tomato maven deploy and added test page for register REST services

---
+++ jamit-logic/src/main/java/se/diversify/jamit/resources/RootResource.java
@@ -0,0 +1,21 @@
+package se.diversify.jamit.resources;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Service shown at the root / of the application, allows testing the rest services
+ */
+@Path("/")
+public class RootResource {
+
+    @GET
+    public String root() throws IOException {
+        return IOUtils.toString(getClass().getResourceAsStream("/root.html"));
+    }
+}
